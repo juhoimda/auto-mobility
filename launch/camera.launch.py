02d45dccfe35bb0d990e8860d46d3a2070deec12
KTHYEONG <115347767+KTHYEONG@users.noreply.github.com>
2026-07-27 14:43:04 +0900

    fix: 카메라 옵션 수정

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -13,8 +13,8 @@ def generate_launch_description():
                 os.path.join(realsense_launch_dir, 'launch', 'rs_launch.py')
             ),
             launch_arguments={
-                'enable_sync': 'false',
-                'align_depth.enable': 'false',
+                'enable_sync': 'true',
+                'align_depth.enable': 'true',
                 'depth_module.depth_profile': '640x480x15',
                 'rgb_camera.color.profile': '640x480x15',
                 'pointcloud.enable': 'false',
