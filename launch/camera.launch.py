18fe72fc775b12992c1d7cfe49b529a286baa2be
KTHYEONG <115347767+KTHYEONG@users.noreply.github.com>
2026-07-28 13:12:33 +0900

    fix: infra 실제 명칭으로 off 수정

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -12,13 +12,15 @@ def generate_launch_description():
             parameters=[{
                 'initial_reset': True,
                 'enable_sync': False,
+                'enable_infra': False,
                 'enable_infra1': False,
                 'enable_infra2': False,
                 'align_depth': {
                     'enable': False
                 },
                 'depth_module': {
-                    'depth_profile': '640x480x15'
+                    'depth_profile': '640x480x15',
+                    'infra_profile': '640x480x15'
                 },
                 'rgb_camera': {
                     'color_profile': '640x480x15'
