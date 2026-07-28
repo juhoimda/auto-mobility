d99c93706a318df372673404b8eccfb740c8b51c
KTHYEONG <115347767+KTHYEONG@users.noreply.github.com>
2026-07-28 12:57:49 +0900

    fix: 파라미터 바인딩 수정

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -1,29 +1,36 @@
-import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    depth_profile_arg = DeclareLaunchArgument('depth_profile', default_value='640x480x15')
+    rgb_profile_arg = DeclareLaunchArgument('rgb_profile', default_value='640x480x15')
+
     return LaunchDescription([
+        depth_profile_arg,
+        rgb_profile_arg,
         Node(
             package='realsense2_camera',
             executable='realsense2_camera_node',
             name='camera',
             namespace='camera',
             output='screen',
-            ros_arguments=[
-                '-p', 'enable_sync:=false',
-                '-p', 'align_depth.enable:=true',
-                '-p', 'depth_module.profile:=640x480x15',
-                '-p', 'depth_module.depth_profile:=640x480x15',
-                '-p', 'rgb_camera.profile:=640x480x15',
-                '-p', 'rgb_camera.color_profile:=640x480x15',
-                '-p', 'pointcloud.enable:=false',
-                '-p', 'enable_accel:=true',
-                '-p', 'enable_gyro:=true',
-                '-p', 'unite_imu_method:=1',
-                '-p', 'gyro_fps:=200',
-                '-p', 'accel_fps:=100'
-            ]
+            parameters=[{
+                'enable_sync': False,
+                'align_depth.enable': True,
+                'depth_module.profile': LaunchConfiguration('depth_profile'),
+                'depth_module.depth_profile': LaunchConfiguration('depth_profile'),
+                'rgb_camera.profile': LaunchConfiguration('rgb_profile'),
+                'rgb_camera.color_profile': LaunchConfiguration('rgb_profile'),
+                'rgb_camera.color.profile': LaunchConfiguration('rgb_profile'),
+                'pointcloud.enable': False,
+                'enable_accel': True,
+                'enable_gyro': True,
+                'unite_imu_method': 1,
+                'gyro_fps': 200,
+                'accel_fps': 100
+            }]
         )
     ])
 
